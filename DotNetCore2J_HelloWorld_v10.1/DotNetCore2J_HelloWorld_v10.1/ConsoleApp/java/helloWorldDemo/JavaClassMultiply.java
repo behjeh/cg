1979381fcffd6611a7a25e10514a572a18f3a2f4
behjeh <44625633+behjeh@users.noreply.github.com>
2020-03-03 10:07:24 -0600

    Add files via upload

---
+++ DotNetCore2J_HelloWorld_v10.1/DotNetCore2J_HelloWorld_v10.1/ConsoleApp/java/helloWorldDemo/JavaClassMultiply.java
@@ -0,0 +1,13 @@
+
+package helloWorldDemo;
+
+public class JavaClassMultiply
+{
+			
+	public void multiply(int a, int b) 
+	{	
+		System.out.println("Hello World from Java Multiply!");
+		System.out.println(a * b);	
+	}
+
+}
